9c896e1c920c288900397c94b8296f094a534bc3
Eyes0fTheWorld <Kscheller@gmail.com>
2020-06-22 21:40:28 -0700

    finished form

--- frontEnd/static/js/script.js
+++ frontEnd/static/js/script.js
@@ -5,3 +5,7 @@ $('.select2').select2();
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
+
+d3.select("#submitButton").on("click", function() {
+    d3.select.("#finalAmount").style("display", "block");
+});
